cca1f9dce9c03264f8ebfe88da18decdab066605
Sued Nepomuceno <sued.muniz.1@gmail.com>
2024-01-30 17:08:53 -0300

    Defining initial configs.

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,14 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  devServer: {
+    proxy: {
+      '^/backendAPI': {
+        target: 'http://localhost:5000',
+        changeOrigin: true,
+        logLevel: 'debug',
+        pathRewrite: { '^/backendAPI': '/' }
+      }
+    }
+  }
 })
